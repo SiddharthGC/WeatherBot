c6c4947886f0784f0129c94b2699d70b47a1c084
Siddharth Chandrasekar <siddharth.gck@gmail.com>
2019-09-28 00:48:02 -0500

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=weatherbotz-Web-Deploy.pubxml -p:Password=BzroileC4gtEM1PbGhTzhAm9L7rW4tsrJFaW9cNvfiALt07nq0Siim617tm6
-
